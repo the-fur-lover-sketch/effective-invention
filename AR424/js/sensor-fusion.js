7a8bc22e73307652ae7a228007363f13292603c1
the-fur-lover-sketch <ykj43313@gmail.com>
2026-04-24 14:50:29 +0900

    add AR424/js/sensor-fusion.js

--- AR424/js/sensor-fusion.js
+++ AR424/js/sensor-fusion.js
@@ -1,1 +1,54 @@
 
+
+/**
+ * sensor-fusion.js
+ * 姿勢（Rotation）の状態管理と、入力（スライダー/センサー）の統合を行う
+ */
+
+
+class SensorFusion {
+    constructor() {
+        //現在の姿勢（Degrees)
+        this.rotation = { x: 0, y: 0, z: 0 };
+
+        //HTMLのスライダー要素
+        this.sliders = {
+            x: document.getElementById('debug-x'),
+            y: document.getElementById('debug-y'),
+            z: document.getElementById('debug-z')
+        };
+
+        this.init();
+    }
+
+    init() {
+        //スライダーのイベントリスナー
+        const updateFromSliders = () => {
+            this.rotation.x = parseFloat(this.sliders.x.value);
+            this.rotation.y = parseFloat(this.sliders.y.value);
+            this.rotation.z = parseFloat(this.sliders.z.value);
+
+            // console.log("Updated from sliders:", this.rotation);
+        };
+        
+        
+        if (this.sliders.x) {
+            this.sliders.x.addEventListener('input', updateFromSliders);
+            this.sliders.y.addEventListener('input', updateFromSliders);
+            this.sliders.z.addEventListener('input', updateFromSliders);
+        }
+
+        //TODO: ジャイロスコープのリスナーもここで設定する
+    }
+
+    /**
+     * 最新の回転データを取得
+     * @returns {Object} {x, y, z}
+     */
+    getRotation() {
+        return this.rotation;
+    }
+}
+
+//renderer.jsからアクセスできるようにする
+window.sensorFusion = new SensorFusion();
