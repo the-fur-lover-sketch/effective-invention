4f48a9e2b828e003a7e9a52e5e185325e18e24b8
the-fur-lover-sketch <ykj43313@gmail.com>
2026-06-04 21:34:51 +0900

    3D描画 index.html main.jsの途中

---
+++ TKGO/main.js
@@ -0,0 +1,20 @@
+//main.js
+import { NavDataStore } from './NavDataStore.js';
+import { NavUIController } from './NavUI.js';
+
+import { initNavViewer, renderNavViewer } from './NavViewer.js';
+
+window.addEventListener('DOMContentLoaded', () => {
+
+    // Initialize the data
+    const dataStore = new NavDataStore();
+
+    // Initialize the renderer
+    initNavViewer('canvas-container');
+
+    //Initialize the UI
+    const ui = new NavUIController((x, z, heading) => {
+        dataStore.updateData(x, z, heading);
+    });
+    
+})
