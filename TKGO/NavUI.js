f5d5a66985089cbf7230d70a4a804b3c1e4196d3
the-fur-lover-sketch <ykj43313@gmail.com>
2026-06-06 13:05:54 +0900

    NavUI complete

--- TKGO/NavUI.js
+++ TKGO/NavUI.js
@@ -1,6 +1,15 @@
 export class NavUIController {
     constructor(onUpdateCallback) {
         this.onUpdateCallback = onUpdateCallback;
-        //const inputs = []
+        const inputs = ['posX' , 'posZ' , 'heading'].map(id => document.getElementById(id));
+        inputs.forEach(input => {
+            if (input) input.addEventListener('input', () => this.handleInput());
+        });
+    }
+    handleInput() {
+        const x = document.getElementById('posX').value;
+        const z = document.getElementById('posZ').value;
+        const heading = document.getElementById('heading').value;
+        this.onUpdateCallback(x, z, heading);
     }
 }
